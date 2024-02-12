5c10eaf237732d15c005a61b84baf9df31fca477
Pierre Bretéché <pbreteche@dawan.fr>
2024-02-12 17:05:54 +0100

    cache tag

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,11 +3,12 @@
 namespace App\Controller;
 
 use App\Service\StatsProcessor;
-use Psr\Cache\CacheItemInterface;
-use Psr\Cache\CacheItemPoolInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\Cache;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Contracts\Cache\ItemInterface;
+use Symfony\Contracts\Cache\TagAwareCacheInterface;
 
 #[Route('/')]
 class DefaultController extends AbstractController
@@ -21,18 +22,29 @@ class DefaultController extends AbstractController
     }
 
     #[Route('/admin/global-stats/{year<\d{4}>}')]
+    #[Cache(expires: '+1 hour')]
     public function heavyProcess(
         StatsProcessor $processor,
-        CacheItemPoolInterface $pool,
+        TagAwareCacheInterface  $pool,
         int $year = null,
     ): Response {
         $year ??= date('Y');
 
-        $stats = $pool->get(sprintf('app_default_heavyprocess_%04d', $year), function (CacheItemInterface $item) use ($processor) {
+        $stats = $pool->get(sprintf('app_default_heavyprocess_%04d', $year), function (ItemInterface $item) use ($processor) {
             $item->expiresAfter(new \DateInterval('PT1H'));
+            $item->tag('heavy_process');
 
             return $processor->compute();
         });
+
+        /* Autres caches :
+         *
+         * Doctrine Registry garde les entités durant une exécution
+         * Doctrine Query Cache : cache le DQL produit par le query builder
+         * HTTP Cache
+         * config symfony, template twig, ...
+         */
+        $pool->invalidateTags(['heavy_process']);
 
         return $this->json(['result' => $stats]);
     }
