1c896132804b90d1d40298415d512a5e96f7ebf3
Pierre Bretéché <pbreteche@dawan.fr>
2024-02-12 13:52:44 +0100

    Session locale event subscriber

---
+++ src/EventSubscriber/LocaleSubscriber.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+class LocaleSubscriber implements EventSubscriberInterface
+{
+    public function onKernelRequest(RequestEvent $event): void
+    {
+        $request = $event->getRequest();
+        if (!$request->hasPreviousSession()) {
+            return;
+        }
+        if ($local = $request->getSession()->get('_locale'))
+        {
+            $request->setLocale($local);
+        }
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            KernelEvents::REQUEST => [['onKernelRequest', 20]],
+        ];
+    }
+}
